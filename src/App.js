4b1508a1c791917beda6ce764fca883536dd6b91
jackeeeeeee <elkaratjack@gmail.com>
2019-10-13 14:50:02 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './assets/vendor/fontawesome-free/css/all.min.css'
+import './assets/css/sb-admin.css'
+
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Index from "./pages/index";
+import AddPage from "./pages/add";
+import EditPage from "./pages/edit";
+import Register from "./pages/register";
+import NotFound from "./pages/notfound";
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Login} />
+                        <Route path='/dashboard' component={Dashboard} />
+                        <Route path='/index' component={Index}/>
+                        <Route path='/register' component={Register} />
+                        <Route path='/add' component={AddPage} />
+                        <Route path='/edit/' component={EditPage} />
+                        <Route path='*' component={NotFound} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/elements/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Link, Redirect} from "react-router-dom";
+import TitleComponent from "../pages/title";
+
+
+export default class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClickLogout = this.handleClickLogout.bind(this)
+    }
+
+    state = {
+        toDashboard: false,
+    };
+
+    handleClickLogout(){
+        localStorage.removeItem('token');
+        localStorage.setItem('isLoggedIn', false);
+        this.setState({ toDashboard: true });
+    }
+
+    render() {
+        if (this.state.toDashboard === true) {
+            return <Redirect to='/' />
+        }
+        return (
+            <nav className="navbar navbar-expand navbar-dark bg-dark static-top">
+                <TitleComponent title="Login"></TitleComponent>
+                <Link to={'/'} className="navbar-brand mr-1">AnyVision</Link>
+            </nav>
+        );
+    }
+}
